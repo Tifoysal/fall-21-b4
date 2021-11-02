e5022857892e9f79959d0b3de1e5346deb9fabdf
Tifoysal <foysal11203004@gmail.com>
2021-11-02 13:28:50 +0600

    master templating

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('master');
 });
+
+Route::get('/admin/orders',[OrderController::class,'orderList']);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
